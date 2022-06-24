437578bccdb54473cbdd2a79960b54ee62241d0a
Delouxeon <88338007+Delouxeon@users.noreply.github.com>
2022-06-24 16:40:12 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let myName = "Nwabuife Divine";
+let myHeight = "72meters";
+let myCountry = "Nigeria";
+
+document.write("My name is ",myName);
+document.write("<br>")
+document.write("I am from ", myCountry);
+document.write("<br>")
+document.write("I am ", myHeight," tall");
